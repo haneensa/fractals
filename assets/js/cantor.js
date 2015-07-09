20fa640b5cb8c8ff44b83a851d3053131da27ee6
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-07-09 22:41:05 +0300

    add structure to the page

--- assets/js/cantor.js
+++ assets/js/cantor.js
@@ -1,13 +1,15 @@
+// fillRect(x, y, width, height)
+// strokeRect(x, y, width, height)
+// clearRect(x, y, width, height)
+
 var c = document.getElementById("cantor");
-var ctx = c.getContext("2d");
- 
-ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
-
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
-
-ctx.font = "30px Arial";
-ctx.fillText("Hello World",10,50); 
+  var ctx = c.getContext('2d');
+for (var i=0;i<6;i++){
+      for (var j=0;j<6;j++){
+        ctx.strokeStyle = 'rgb(0,' + Math.floor(255-42.5*i) + ',' + 
+                         Math.floor(255-42.5*j) + ')';
+        ctx.beginPath();
+        ctx.arc(12.5+j*25,12.5+i*25,10,0,Math.PI*2,true);
+        ctx.stroke();
+      }
+    }
