31bc99b36623b5ba9861616af7d7b75d13463d67
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-07-10 02:47:15 +0300

    cantor set

---
+++ assets/js/koch.js
@@ -0,0 +1,17 @@
+
+var c = document.getElementById("koch");
+var ctx = c.getContext('2d');
+
+for (var x = 0.5; x < 550; x += 10) {
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, 450);
+}
+
+for (var y = 0.5; y < 450; y += 10) {
+  ctx.moveTo(0, y);
+  ctx.lineTo(550, y);
+}
+
+ctx.strokeStyle = "#eee";
+ctx.stroke();
+ 
