aaa2401e747465a908a447ddd3a47138ae055f23
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-07-09 18:52:58 +0300

    cantor

---
+++ _site/assets/js/cantor.js
@@ -0,0 +1,13 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ 
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
+
+ctx.font = "30px Arial";
+ctx.fillText("Hello World",10,50); 
