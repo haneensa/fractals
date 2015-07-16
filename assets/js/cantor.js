59dec80c03f9304b92dd4e0cf91dfaae9dc65f99
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-07-16 21:54:30 +0300

    adds

--- assets/js/cantor.js
+++ assets/js/cantor.js
@@ -4,53 +4,24 @@
 // arc(x, y, r, sAngle, eAngle, counterclockwise)
 // moveTo(x, y) moves the pencil to the specified starting point
 // lineTo(x, y) draws a line to the specified ending point
-
-/*
-function draw() {
-	var c = document.getElementById("cantor");
-  var ctx = c.getContext('2d');
-for (var i=0;i<6;i++){
-      for (var j=0;j<6;j++){
-        ctx.strokeStyle = 'rgb(0,' + Math.floor(255-42.5*i) + ',' + 
-                         Math.floor(255-42.5*j) + ')';
-        ctx.beginPath();
-        ctx.arc(12.5+j*25,12.5+i*25,10,0,Math.PI*2,true);
-        ctx.stroke();
-      }
-    }
-}
-*/
-
-var c = document.getElementById("cantor");
-  var ctx = c.getContext('2d');
-
-for (var x = 0.5; x < 550; x += 10) {
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, 450);
-}
-
-for (var y = 0.5; y < 450; y += 10) {
-  ctx.moveTo(0, y);
-  ctx.lineTo(550, y);
-}
-
-ctx.strokeStyle = "#eee";
-ctx.stroke();
- 
-
-function drawcircle(x, y, r) {
-	ctx.beginPath();
-	ctx.strokeStyle = "#1DBBCB";
-	ctx.arc(x,y,r,0,Math.PI*2,true); // Outer circle
-	ctx.stroke();
-	if (r > 2) {
-		drawcircle(x + r/2, y, r/2);
+// fillText('text', x, y)
+// double buffering
+function layout(ctx) {
+	for (var x = 0.5; x < 550; x += 10) {
+	  ctx.moveTo(x, 0);
+	  ctx.lineTo(x, 450);
 	}
 
-}  
-//drawcircle(200, 100, 100);
- 
-function line(x, y, len) {
+	for (var y = 0.5; y < 450; y += 10) {
+	  ctx.moveTo(0, y);
+	  ctx.lineTo(550, y);
+	}
+
+	ctx.strokeStyle = "#eee";
+	ctx.stroke();
+} 
+
+function line(ctx, x, y, len) {
 	ctx.beginPath();
 	ctx.moveTo(x, y);
  	ctx.lineTo(x + len, y);
@@ -58,14 +29,23 @@ function line(x, y, len) {
  	ctx.stroke();
 }
  
-function cantor(x, y, len) {
+function cantor(ctx, x, y, len) {
 	if (len >= 1) {
-		line(x, y, len);
+		line(ctx, x, y, len);
 		y += 20;
 
-		cantor(x, y, len/3);
-		cantor(x+len*2/3, y, len/3);
+		cantor(ctx, x, y, len/3);
+		cantor(ctx, x+len*2/3, y, len/3);
 	}
 }
 
-cantor(10, 200, 530);
+
+var canvas = document.getElementById("cantor");
+var context = null;
+
+if (canvas && canvas.getContext('2d') ){
+	context = canvas.getContext('2d');
+	layout(context);
+	cantor(context, 10, 200, 530);
+}
+
--- assets/js/koch.js
+++ assets/js/koch.js
@@ -15,3 +15,22 @@ for (var y = 0.5; y < 450; y += 10) {
 ctx.strokeStyle = "#eee";
 ctx.stroke();
  
+function circle(x, y, r) {
+	ctx.beginPath();
+	ctx.strokeStyle = "#1DBBCB";
+	ctx.arc(x,y,r,0,Math.PI*2,true); // Outer circle
+	ctx.stroke();
+}
+
+function drawcircle(x, y, r) {
+	circle(x, y, r);
+	if (r > 8) {
+		drawcircle(x + r/2, y, r/2);
+		drawcircle(x - r/2, y, r/2);
+		drawcircle(x , y + r/2, r/2);
+		drawcircle(x, y - r/2, r/2);
+	}
+
+}  
+drawcircle(550/2, 450/2, 200);
+ 
