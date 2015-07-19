d6cd129a64277c1ff4fb7840ed95a864c7e930da
Haneen Mohammed <hamohammed.sa@gmail.com>
2015-07-19 03:14:09 +0300

    mouse control

--- assets/js/cantor.js
+++ assets/js/cantor.js
@@ -6,20 +6,45 @@
 // lineTo(x, y) draws a line to the specified ending point
 // fillText('text', x, y)
 // double buffering
-function layout(ctx) {
-	for (var x = 0.5; x < 550; x += 10) {
-	  ctx.moveTo(x, 0);
-	  ctx.lineTo(x, 450);
-	}
 
-	for (var y = 0.5; y < 450; y += 10) {
-	  ctx.moveTo(0, y);
-	  ctx.lineTo(550, y);
-	}
+var canvas = document.getElementById("cantor");
+var context = null;
+var p = 3;
 
-	ctx.strokeStyle = "#eee";
-	ctx.stroke();
-} 
+function init() {
+    if (canvas && canvas.getContext('2d') ) {
+        context = canvas.getContext('2d');
+        context.clearRect(0, 0, 500, 450);
+        background(500, 550, context);
+        cantor(context, 10, 200, 530);
+    }
+}
+canvas.addEventListener("click", update, false);
+
+function update(e) {
+    p++;
+    p %= 6;
+    alert("alet");
+    init();
+}
+
+
+init();
+
+function background(width, height, ctx) {
+    for (var x = 0.5; x < height; x += 10) {
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, width);
+    }
+
+    for (var y = 0.5; y < width; y += 10) {
+        ctx.moveTo(0, y);
+        ctx.lineTo(height, y);
+    }
+
+    ctx.strokeStyle = "#eee";
+    ctx.stroke();
+}
 
 function line(ctx, x, y, len) {
 	ctx.beginPath();
@@ -30,22 +55,13 @@ function line(ctx, x, y, len) {
 }
  
 function cantor(ctx, x, y, len) {
-	if (len >= 1) {
-		line(ctx, x, y, len);
-		y += 20;
+	if (len < 1) {
+	    return null
+    }
 
-		cantor(ctx, x, y, len/3);
-		cantor(ctx, x+len*2/3, y, len/3);
-	}
+    line(ctx, x, y, len);
+    y += 20;
+
+    cantor(ctx, x, y, len/p);
+    cantor(ctx, x+len*2/3, y, len/p);
 }
-
-
-var canvas = document.getElementById("cantor");
-var context = null;
-
-if (canvas && canvas.getContext('2d') ){
-	context = canvas.getContext('2d');
-	layout(context);
-	cantor(context, 10, 200, 530);
-}
-
--- assets/js/koch3.js
+++ assets/js/koch3.js
@@ -1,46 +1,30 @@
 //http://processingjs.org/learning/topic/koch/
 var c = document.getElementById("koch");
 var context = c.getContext('2d');
-
-function background(width, height, ctx) {
-    for (var x = 0.5; x < height; x += 10) {
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, width);
-    }
-
-    for (var y = 0.5; y < width; y += 10) {
-        ctx.moveTo(0, y);
-        ctx.lineTo(height, y);
-    }
-
-    ctx.strokeStyle = "#eee";
-    ctx.stroke();
+var ctx = context;
+for (var x = 0.5; x < 550; x += 10) {
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, 500);
 }
 
-var depth = 0;
+for (var y = 0.5; y < 500; y += 10) {
+  ctx.moveTo(0, y);
+  ctx.lineTo(550, y);
+}
 
-
+ctx.strokeStyle = "#eee";
+ctx.stroke();
 
 function init(){
-    context.clearRect(0, 0, 500, 550);
-    background(500, 550, context);
     context.beginPath();
     context.stroke();
     context.closePath();
-    fractal([50,150], [500,150], depth);
-    fractal([270,490], [50,150], depth);
-    fractal([500,150],[270,490], depth);
+    fractal([50,150], [500,150], 5);
+    fractal([270,490], [50,150],5);
+    fractal([500,150],[270,490],5);
+
 };
 
-c.addEventListener("mousemove", update, false);
-
-function update(e){
-    depth++ ;
-    depth %= 6;
-    init();
-}
-
-init();
 function fractal(A, B, depth){
     if (depth < 0){
         return null;                
@@ -54,7 +38,7 @@ function fractal(A, B, depth){
 
     var E = add(multiply(V2, Math.sqrt(3)/6 * length(B, A)), F);
 
-    DrawLine(A, B, "#1EBAAB");
+    DrawLine(A, B, "black");
 
     if (depth !=0){
         for (var i=0;i<10;i++)
@@ -97,3 +81,4 @@ function DrawLine(a, b, c){
     context.closePath();
 };
 
+init();
